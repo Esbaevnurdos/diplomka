b16d235e41966ea92f4222518e50f608c21ce118
Esbaevnurdos <nurdosesbaev82@gmail.com>
2025-06-07 22:51:44 +0500

    getallpatients

--- db/queries.js
+++ db/queries.js
@@ -600,12 +600,27 @@ const addPatient = async (
 };
 
 const getAllPatients = async () => {
-  const query = `SELECT * FROM patients ORDER BY appointment_date_time DESC;`;
+  const query = `
+    SELECT 
+      p.id,
+      p.name,
+      p.payment_type,
+      p.appointment_date_time,
+      p.comment,
+      p.created_at,
+      p.updated_at,
+      s.title AS service,
+      sp.name AS specialist
+    FROM patients p
+    LEFT JOIN services s ON p.service = s.id
+    LEFT JOIN specialists sp ON p.specialist = sp.id
+    ORDER BY p.appointment_date_time DESC;
+  `;
   try {
     const result = await db.query(query);
     return result.rows;
   } catch (error) {
-    console.error("Error fetching patients:", error.message);
+    console.error("Error fetching patients:", error);
     throw error;
   }
 };
